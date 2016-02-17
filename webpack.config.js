9946dddc970356c0637b154235643f614bbd1ea7
Matthew Henkler <matt@henkler.org>
2016-02-17 08:33:35 -0600

    Initial changes for webpack before dev/prod split

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,18 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
+    'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/app'
+    './src/app',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     loaders: [
@@ -27,6 +26,10 @@ module.exports = {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass'],
       },
-    ]
-  }
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
 };
